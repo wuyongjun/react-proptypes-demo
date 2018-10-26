ca0d7d7e004889f5053b957e7db2a8962c968041
wuyongjun <wuyongjuntie2013@163.com>
2018-10-26 13:35:05 +0800

    add code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
+import PropTypes from 'prop-types';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+    // es7 静态属性
+    static propTypes = {
+        name: PropTypes.number
+    }
+    static defaultProps = {
+        name: 1024
+    }
+    render () {
+        return (<div>
+            <h3>welcome to react</h3>
+            <p>{this.props.name}</p>
+        </div>)
+    }
+}
+
+// App.propTypes = {
+//     name: PropTypes.number
+// };
+
+// App.defaultProps = {
+//     name: 1024
+// }
+
+ReactDOM.render(<App name={'hello'} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
